87f66b5d64d2a6bdaa0b2fcad284631ee3538b16
Luke Marshall <lukemarshall2222@gmail.com>
2023-10-18 12:26:57 -0700

    add status checking to classes

--- dpll.py
+++ dpll.py
@@ -14,7 +14,7 @@ class DPLL(object):
         if isinstance(clause, set): # negating a clause produces separted negated literals that must be added individually
             while len(clause != 0):
                 self.clauses.append(clause.pop())
-        else: 
+        else:
             self.proposition.append(clause)
 
     def sat(self):
@@ -24,9 +24,25 @@ class DPLL(object):
 class Clause(object):
 
     def __init__(self, *args) -> None:
-        self.clause = [] 
+        self.clause = []
+        self.__status = None
         for arg in args:
             self.clause.append(arg)
+
+    def set_status(self):
+        val = False
+        for literal in self.clause:
+            if literal.get_status():
+                val = True
+        self.__status = val
+
+    def get_status(self):
+        self.set_status()
+        return self.__status
+
+    def ADD(self, item: 'Literal'):
+        self.clause.append(item)
+        self.set_status()
 
 
     def NOT(self):
@@ -35,7 +51,15 @@ class Clause(object):
             c) ~(a v b) = ~a ∧ ~b"""
         negated = set()
         for instance in self.clause:
+            try: 
+                if isinstance(instance, Clause):
+                    raise Exception
+            except Exception:
+                print('An attempt was made to negate a nested clause which would lead to the outer clause not being in conjunctive normal form.' 
+                      + 'Simplify or rearrange to avoid negating a nested clause')
+                raise Exception
             negated.add(instance.NOT())
+        self.set_status()
         return negated
 
         
@@ -44,6 +68,7 @@ class Literal(object):
     def __init__(self, literal) -> None:
         self.variable = str(literal)
         self.sign = "+"
+        self.__status = None
 
     def __str__(self) -> str:
         return self.sign + f"{self.variable}"
@@ -61,3 +86,12 @@ class Literal(object):
     
     def is_pos(self):
         return self.sign == '+'
+    
+    def set_status(self, val: bool):
+        self.__status = val
+
+    def get_status(self):
+        return self.__status
+
+    def __eq__(self, literal1: 'Literal', literal2: 'Literal') -> bool:
+        return literal1.variable == literal2.variable
