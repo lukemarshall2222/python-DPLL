db131247c591255a0a2ffe69d74735424efdac21
Luke Marshall <lukemarshall2222@gmail.com>
2023-11-03 11:12:11 -0700

    add working hueristics

--- dpll.py
+++ dpll.py
@@ -1,14 +1,80 @@
 """
+
 Luke Marshall
 DPLL solver
 
 ANDs are implicit in the dpll array between each of the clauses, ORs are implicit in the clause arrays
 """
 
+class Literal(object):
+
+    def __init__(self, literal) -> None:
+        self.__variable = str(literal)
+        self.sign = "+"
+        self.__status = None
+        self.__calculated_val = None
+
+    def __str__(self) -> str:
+        return self.sign + f"{self.get_variable()}"
+    
+    def NOT(self):
+        # ~(~a) == a
+        # ~(a) == ~a
+        self.sign = '-' if self.sign == '+' else ''
+
+    def get_variable(self):
+        return self.__variable
+    
+    def get_sign(self):
+        return 'pos' if self.sign == '+' else 'neg'
+    
+    def is_pos(self):
+        return self.__calculated_val
+    
+    def set_status(self, val=True):
+        try:
+            if not isinstance(val, bool):
+                raise TypeError
+        except TypeError:
+            print('set_status only takes a boolean value as an argument.')
+            raise TypeError
+        self.__status = val
+
+    def get_status(self):
+        return self.__status
+    
+    def get_calculated_val(self):
+        return self.__calculated_val
+    
+    def set_calculated_val(self):
+        if self.__status == True:
+            self.__calculated_val = self.sign == '+'
+        elif self.__status == False:
+            self.__calculated_val = self.sign == '-'
+        else: 
+            self.__calculated_val = None
+
+    def __eq__(self, other: 'Literal',) -> bool:
+        return self.get_variable() == other.get_variable()
+
+
 class DPLL(object):
 
-    def __init__(self) -> None:
-        self.proposition = []
+    def __init__(self, *args) -> None:
+        self.variables = {}
+        self.proposition = [arg for arg in args]
+        for clause in self.proposition:
+            if not isinstance(clause, Literal) and not isinstance(clause, Clause):
+                print("The proposition only takes Literal and Clause objects.")
+                raise TypeError
+            elif isinstance(clause, Literal):
+                if not clause.get_variable() in self.variables:
+                    self.variables[clause.get_variable()] = None
+            else:
+                for lit in clause:
+                    if not lit.get_variable() in self.variables:
+                        self.variables[lit.get_variable()] = None
+
 
     def ADD(self, clause):
         if isinstance(clause, set): # negating a clause produces separted negated literals that must be added individually
@@ -17,13 +83,35 @@ class DPLL(object):
                 if not isinstance(x, Clause) and not isinstance(x, Literal):
                     print("DPLL proposition can only be made up of Literal and Clause objects.")
                     raise TypeError
-                self.clauses.append(x)
-        elif isinstance(clause, Literal) or isinstance(clause, Clause):
-
-            self.proposition.append(clause) # a single clause must be added to a proposition directly
+                x_var = x.get_variable()
+                if not x_var in self.variables:
+                    self.variables[x_var] = None
+                self.proposition.append(x)
+        elif isinstance(clause, Literal):
+            if not clause.get_variable() in self.variables:
+                self.variables[clause.get_variable()] = None
+            self.proposition.append(clause) # add the literal directly to the proposition
+        elif isinstance(clause, Clause):
+            for lit in clause:
+                if not lit.get_variable() in self.variables:
+                    self.variables[lit.get_variable()] = None
+            self.proposition.append(clause) # add the clause directly to the proposition
         else:
             print("DPLL proposition can only be made up of Literal and Clause objects.")
             raise TypeError
+        
+    def remove(self, clause):
+        if clause in self.proposition:
+            self.proposition.remove(clause)
+        else:
+            return False
+        return True
+    
+    def __iter__(self):
+        return iter(self.proposition)
+    
+    def __getitem__(self, index):
+        return self.proposition[index]
 
     def sat(self):
         """Returns if the proposition is solvable or not"""
@@ -44,111 +132,114 @@ class DPLL(object):
         
         # apply unit clause heristic
         res = self.unit_clause_heuristic()
-        if res == "unsat":
-            return res
-        elif res == "changed":
+        if res:
             return sat()
         
         res2 = self.pure_clause_hueristic()
-        if res2 == "cahnged":
-            return sat()
+        if res2:
+            return sat() 
+        
+        prop_cp = self.proposition.copy()
+        least = len(self.proposition[0])
+        guess = self.proposition[0][0].get_variable()
+        for clause in self.proposition:
+            if len(clause) < least:
+                least = len(clause)
+                guess = clause[0].get_variable()
+        for clause in self.proposition:
+            for lit in clause:
+                if lit.get_variable() == guess:
+                    lit.set_status()
+        res3 = sat()
+        if res3 == 'sat':
+            return res3
+        else:
+            self.proposition = prop_cp
+        for clause in self.proposition:
+            for lit in clause:
+                if lit.get_variable() == guess:
+                    lit.set_status(False)
+        return sat()
+
+
+    def pure_clause_hueristic(self) -> bool:
+        # check if a variable shows up only in its positive or negative form in the proposition
+        changed = False
+        var_signs = {}
+        var_uniformity = {}
+        for clause in self.proposition:
+            assert isinstance(clause, Clause), "non-Clause found during pure clause check"
+            for lit in clause:
+                assert isinstance(lit, Literal), "non-Literal found within a clause"
+                lit_var = lit.get_variable()
+                lit_sign = lit.get_sign()
+                if lit_var in var_signs:
+                    var_uniformity[lit_var] = False if var_signs[lit_var] == lit_sign else var_uniformity[lit_var]
+                else:
+                    var_signs[lit_var] = lit_sign
+                    var_uniformity[lit_var] = True
+        for clause in self.proposition:
+            for lit in clause:
+                assert isinstance(lit, Literal)
+                lit_var = lit.get_variable()
+                lit_sign = lit.get_sign()
+                if var_uniformity[lit_var]:
+                    self.variables[lit_var] = True if lit_sign == 'pos' else 'False'
+                    self.proposition.remove(clause)
+                    break
+        return changed
         
 
-    def guess(self):
-        clause_var = ''
-        for i in range(len(self.proposition)):
-            for clause in self.proposition:
-                if len(clause) <= i:
-                    clause.set 
 
-
-    def pure_clause_hueristic(self):
-        # check if a variable shows up only in its positive or negative form in the proposition
-        literals_dict = {}
-        for clause in self.proposition:
-            if isinstance(clause, Literal):
-                if clause.get_variable() in literals_dict:
-                    if literals_dict[clause][0] == True:
-                        if literals_dict[clause][1] != clause.get_sign():
-                            literals_dict[clause][0] = False
-                else:
-                    literals_dict[clause] = (True, clause.get_sign())
-            else:
-                for literal in clause:
-                    if literal.get_variable() in literals_dict:
-                        if literals_dict[literal][0] == True:
-                            if literals_dict[literal][1] != clause.get_sign():
-                                literals_dict[literal][0] = False
-                else:
-                    literals_dict[literal] = (True, clause.get_sign())
-
-
-    def unit_clause_heuristic(self)
-        # check the unit if any of the unit clauses are the only time that the literal shows up in the proposition
-        # to see if unit clause heuristic may be implemented
-        unit_clauses = set()
-        for clause in self.proposition:
-            if isinstance(clause, Literal):
-                unit_clauses.add(clause.get_variable())
-        for clause in self.proposition:
-            if isinstance(clause, Clause):
-                for lit in clause:
-                    var = lit.get_variable()
-                    if var in unit_clauses:
-                        unit_clauses.remove(var)
-        if len(unit_clauses) > 0:
-            for lit in unit_clauses:
-                if lit.is_pos():
-                    lit.set_status(True)
-                else:
-                    lit.set_status(False)
-                assert lit.get_calculated_val() == True
-            
-        # check to make sure all the unit clauses with the same variable have the same underlying boolean value
+    def unit_clause_heuristic(self):
+        changed = False
         uclauses = {}
         for clause in self.proposition:
             if isinstance(clause, Literal):
-                var = clause.get_variable()
-                if var in uclauses:
-                    if clause.get_status() != uclauses[var]:
+                lit_var = clause.get_variable()
+                lit_sign = clause.get_sign()
+                if lit_var in uclauses:
+                    if lit_sign != uclauses[lit_var]:
+                        # two unit clauses with same variable but opposite sign 
+                        # unsat by contradiction
                         return 'unsat'
                 else:
-                    uclauses[var] = clause.get_status()
-                    continue
-
-        # if a literal contained within a clause is the positive form of a unit clause, 
-        # this makes the clause true and it may be removed from the proposition
-        changed = False
-        for clause in self.proposition:
-            if isinstance(clause, Literal):
+                    uclauses[lit_var] = lit_sign
+                    self.variables[lit_var] = True if lit_sign == 'pos' else False
                 self.proposition.remove(clause)
                 changed = True
-            elif isinstance(clause, Clause):
-                for literal in clause:
-                    if literal.get_variable() in unit_clauses and literal.is_pos():
-                        self.proposition.remove(clause)
-                        break
-            
+        for clause in self.proposition:
+            if isinstance(clause, Clause):
+                for lit in clause:
+                    assert isinstance(lit, Literal), "This Clause contains an object that is not a Literal."
+                    lit_var = lit.get_variable()
+                    if lit_var in uclauses:
+                        if lit.get_sign() == uclauses[lit_var]:
+                            self.proposition.remove(clause)
+                            break
+                        else:
+                            clause.remove(lit)
+                            continue
         return "changed" if changed else "unchanged"
 
 class Clause(object):
 
     def __init__(self, *args) -> None:
-        self.clause = []
+        self.__clause = []
         self.__status = None
         for arg in args:
             if isinstance(arg, Literal):
-                self.clause.append(arg)
+                self.__clause.append(arg)
             elif isinstance(arg, Clause):
                 for lit in arg:
-                    self.clause.append(lit)
+                    self.__clause.append(lit)
             else:
                 print("Clause object only accepts Literal or Clause objects as input.")
                 raise TypeError
 
     def set_status(self):
         val = False
-        for literal in self.clause:
+        for literal in self.__clause:
             if literal.get_calculated_val():
                 val = True
         self.__status = val
@@ -156,88 +247,49 @@ class Clause(object):
     def get_status(self):
         self.set_status()
         return self.__status
-
+    
     def ADD(self, item):
         if isinstance(item, Literal):
-            self.clause.append(item)
+            self.__clause.append(item)
         elif isinstance(item, Clause):
             if item.is_empty():
                 print("An empty Clause object cannot be added to a Clause object")
                 raise AttributeError
             for lit in item:
-                self.clause.append(lit)
+                self.__clause.append(lit)
         else:
             print("Clause object only accepts Literal or Clause objects as input.")
             raise TypeError
         self.set_status()
+
+    def remove(self, literal):
+        if literal in self.__clause:
+            self.__clause.remove(literal)
+        else:
+            return False
+        return True 
 
     def NOT(self):
         """Move negations inside e.g. :
             a) ~(~a) = a
             c) ~(a v b) = ~a ∧ ~b"""
         negated = set()
-        for instance in self.clause:
+        for instance in self.__clause:
             negated.add(instance.NOT())
         self.set_status()
         return negated
     
     def is_empty(self):
-        return len(self.clause) == 0
+        return len(self.__clause) == 0
     
     def __len__(self):
-        return len(self.clause)
+        return len(self.__clause)
+    
+    def __iter__(self):
+        return iter(self.__clause)
+    
+    def __getitem__(self, index):
+        return self.__clause[index]
 
         
-class Literal(object):
 
-    def __init__(self, literal) -> None:
-        self.__variable = str(literal)
-        self.sign = "+"
-        self.__status = None
-        self.__calculated_val = None
-
-    def __str__(self) -> str:
-        return self.sign + f"{self.variable}"
-    
-    def NOT(self):
-        # ~(~a) == a
-        # ~(a) == ~a
-        self.sign = '-' if self.sign == '+' else ''
-
-    def get_variable(self):
-        return self.__variable
-    
-    def get_sign(self):
-        print('This literal has a sign that is ' + 'positive' if self.sign == '+' else 'negative')
-    
-    def is_neg(self):
-        return self.sign == '-'
-    
-    def is_pos(self):
-        return self.sign == '+'
-    
-    def set_status(self, val: bool):
-        try:
-            if not isinstance(val, bool):
-                raise TypeError
-        except TypeError:
-            print('set_status only take a boolean value as an argument.')
-            raise TypeError
-        self.__status = val
-
-    def get_status(self):
-        return self.__status
-    
-    def get_calculated_val(self):
-        return self.__calculated_val
-    
-    def set_calculated_val(self)
-        if self.__status == True:
-            self.__calculated_val = self.sign == '+'
-        elif self.__status == False:
-            self.__calculated_val = self.sign == '-'
-        else: 
-            self.__calculated_val = None
-
-    def __eq__(self, other: 'Literal',) -> bool:
-        return self.variable == other.variable
